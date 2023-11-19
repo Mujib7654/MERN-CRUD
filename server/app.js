a32228b1496f347c082d8308ef872432d30c70bf
Mujibullah Inami <mujibullahinami26@gmail.com>
2023-11-19 21:56:31 +0530

    fix

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 app.use('/api', createBookRouter);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
